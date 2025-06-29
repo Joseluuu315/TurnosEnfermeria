2ea59351430004a711c65fc9f0496ab56f35c720
José Luis Fuentes Parra <josefnts3115@gmail.com>
2025-06-29 23:28:05 +0200

    Create register.js

---
+++ js/register.js
@@ -0,0 +1,7 @@
+document.getElementById("registerForm").addEventListener("submit", async function(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  const res = await fetch('php/register.php', { method: 'POST', body: formData });
+  if (res.ok) window.location.href = 'index.html';
+  else alert("Error al registrar");
+});
